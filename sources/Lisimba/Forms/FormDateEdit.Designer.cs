61031e5b349de742b38214e5b8f050856a947b40
lastunicorn <coldblackstar@yahoo.com>
2015-07-09 11:52:56 +0300

    i remade the DateEdit form with tables.

--- sources/Lisimba/Forms/FormDateEdit.Designer.cs
+++ sources/Lisimba/Forms/FormDateEdit.Designer.cs
@@ -34,18 +34,26 @@ namespace DustInTheWind.Lisimba.Forms
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.labelDay = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBoxYear
             // 
+            this.textBoxYear.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.textBoxYear.BackColor = System.Drawing.Color.LightYellow;
-            this.textBoxYear.Location = new System.Drawing.Point(215, 28);
+            this.textBoxYear.Location = new System.Drawing.Point(199, 16);
             this.textBoxYear.Name = "textBoxYear";
-            this.textBoxYear.Size = new System.Drawing.Size(65, 20);
+            this.textBoxYear.Size = new System.Drawing.Size(82, 20);
             this.textBoxYear.TabIndex = 2;
             // 
             // comboBoxMonth
             // 
+            this.comboBoxMonth.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.comboBoxMonth.AutoCompleteCustomSource.AddRange(new string[] {
             "1",
             "2",
@@ -83,15 +91,16 @@ namespace DustInTheWind.Lisimba.Forms
             this.comboBoxMonth.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.comboBoxMonth.FormattingEnabled = true;
             this.comboBoxMonth.ItemHeight = 13;
-            this.comboBoxMonth.Location = new System.Drawing.Point(87, 27);
-            this.comboBoxMonth.Margin = new System.Windows.Forms.Padding(1);
+            this.comboBoxMonth.Location = new System.Drawing.Point(73, 16);
             this.comboBoxMonth.MaxDropDownItems = 13;
             this.comboBoxMonth.Name = "comboBoxMonth";
-            this.comboBoxMonth.Size = new System.Drawing.Size(117, 21);
+            this.comboBoxMonth.Size = new System.Drawing.Size(120, 21);
             this.comboBoxMonth.TabIndex = 1;
             // 
             // comboBoxDay
             // 
+            this.comboBoxDay.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.comboBoxDay.AutoCompleteCustomSource.AddRange(new string[] {
             "1",
             "2",
@@ -128,8 +137,7 @@ namespace DustInTheWind.Lisimba.Forms
             this.comboBoxDay.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxDay.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.comboBoxDay.FormattingEnabled = true;
-            this.comboBoxDay.Location = new System.Drawing.Point(12, 27);
-            this.comboBoxDay.Margin = new System.Windows.Forms.Padding(1);
+            this.comboBoxDay.Location = new System.Drawing.Point(3, 16);
             this.comboBoxDay.MaxDropDownItems = 16;
             this.comboBoxDay.Name = "comboBoxDay";
             this.comboBoxDay.Size = new System.Drawing.Size(64, 21);
@@ -139,7 +147,7 @@ namespace DustInTheWind.Lisimba.Forms
             // 
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(212, 10);
+            this.label2.Location = new System.Drawing.Point(199, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(64, 13);
             this.label2.TabIndex = 4;
@@ -149,7 +157,7 @@ namespace DustInTheWind.Lisimba.Forms
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(84, 10);
+            this.label1.Location = new System.Drawing.Point(73, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(120, 13);
             this.label1.TabIndex = 5;
@@ -159,29 +167,57 @@ namespace DustInTheWind.Lisimba.Forms
             // 
             this.labelDay.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelDay.ForeColor = System.Drawing.Color.Black;
-            this.labelDay.Location = new System.Drawing.Point(12, 10);
+            this.labelDay.Location = new System.Drawing.Point(3, 0);
             this.labelDay.Name = "labelDay";
             this.labelDay.Size = new System.Drawing.Size(64, 13);
             this.labelDay.TabIndex = 6;
             this.labelDay.Text = "Day:";
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.labelDay, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.comboBoxMonth, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxYear, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.comboBoxDay, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(4, 4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(284, 48);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(4);
+            this.panel1.Size = new System.Drawing.Size(292, 56);
+            this.panel1.TabIndex = 8;
+            // 
             // FormDateEdit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 61);
-            this.Controls.Add(this.textBoxYear);
-            this.Controls.Add(this.comboBoxMonth);
-            this.Controls.Add(this.comboBoxDay);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.labelDay);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(292, 56);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(308, 90);
             this.Name = "FormDateEdit";
-            this.ShowInTaskbar = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Edit Date";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -193,5 +229,7 @@ namespace DustInTheWind.Lisimba.Forms
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label labelDay;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
