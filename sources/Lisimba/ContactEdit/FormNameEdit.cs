4b96943eb48643abde0ef28ef0420e2ac8f8d978
lastunicorn <coldblackstar@yahoo.com>
2015-07-20 12:17:56 +0300

    added name edit form.

---
+++ sources/Lisimba/ContactEdit/FormNameEdit.cs
@@ -0,0 +1,74 @@
+// Lisimba
+// Copyright (C) 2007-2014 Dust in the Wind
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Windows.Forms;
+using DustInTheWind.Lisimba.Egg.Book;
+
+namespace DustInTheWind.Lisimba.ContactEdit
+{
+    public partial class FormNameEdit : FormEditBase
+    {
+        private PersonName personName;
+        public PersonName PersonName
+        {
+            get { return personName; }
+            set
+            {
+                personName = value;
+                DisplayDataInView();
+            }
+        }
+
+        public FormNameEdit()
+        {
+            InitializeComponent();
+
+            textBoxFirstName.KeyDown += FormEditBase_KeyDown;
+            textBoxMiddleName.KeyDown += FormEditBase_KeyDown;
+            textBoxLastName.KeyDown += FormEditBase_KeyDown;
+            textBoxNickname.KeyDown += FormEditBase_KeyDown;
+        }
+
+        protected override void UpdateData()
+        {
+            bool isAnyDataChanged = !textBoxFirstName.Text.Equals(personName.FirstName) ||
+                     !textBoxMiddleName.Text.Equals(personName.MiddleName) ||
+                     !textBoxLastName.Text.Equals(personName.LastName) ||
+                     !textBoxNickname.Text.Equals(personName.Nickname);
+
+            if (!isAnyDataChanged)
+                return;
+
+            ReadDataFromView();
+        }
+
+        private void DisplayDataInView()
+        {
+            textBoxFirstName.Text = personName.FirstName;
+            textBoxMiddleName.Text = personName.MiddleName;
+            textBoxLastName.Text = personName.LastName;
+            textBoxNickname.Text = personName.Nickname;
+        }
+
+        private void ReadDataFromView()
+        {
+            personName.FirstName = textBoxFirstName.Text;
+            personName.MiddleName = textBoxMiddleName.Text;
+            personName.LastName = textBoxLastName.Text;
+            personName.Nickname = textBoxNickname.Text;
+        }
+    }
+}
---
+++ sources/Lisimba/UserControls/CustomButton.cs
@@ -0,0 +1,90 @@
+﻿// Lisimba
+// Copyright (C) 2007-2014 Dust in the Wind
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using DustInTheWind.Lisimba.Operations;
+using DustInTheWind.Lisimba.Services;
+
+namespace DustInTheWind.Lisimba.UserControls
+{
+    internal class CustomButton : Button
+    {
+        private IOpertion opertion;
+
+        [Browsable(false)]
+        public ApplicationStatus ApplicationStatus { get; set; }
+
+        [Browsable(false)]
+        public IOpertion Opertion
+        {
+            get { return opertion; }
+            set
+            {
+                if (opertion != null)
+                    opertion.IsEnabledChanged -= HandleOpertionEnabledChanged;
+
+                opertion = value;
+
+                if (opertion != null)
+                    opertion.IsEnabledChanged += HandleOpertionEnabledChanged;
+
+                Enabled = opertion == null || opertion.IsEnabled;
+            }
+        }
+
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            if (ApplicationStatus != null)
+            {
+                string description = CalculateTextToDisplayAsStatus();
+                ApplicationStatus.SetPermanentStatusText(description);
+            }
+
+            base.OnMouseEnter(e);
+        }
+
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            if (ApplicationStatus != null)
+                ApplicationStatus.Reset();
+
+            base.OnMouseLeave(e);
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            if (Opertion != null)
+                Opertion.Execute();
+
+            base.OnClick(e);
+        }
+
+        private void HandleOpertionEnabledChanged(object sender, EventArgs eventArgs)
+        {
+            Enabled = opertion.IsEnabled;
+        }
+
+        private string CalculateTextToDisplayAsStatus()
+        {
+            if (Opertion != null && Opertion.ShortDescription != null)
+                return Opertion.ShortDescription;
+
+            return null;
+        }
+    }
+}
