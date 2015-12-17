acf0257f8386bceb990a985a596988c494a63f08
lastunicorn <coldblackstar@yahoo.com>
2015-12-17 20:37:32 +0200

    added the close command class

---
+++ sources/Lisimba.Cmd/Commands/CloseCommand.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace Lisimba.Cmd.Commands
+{
+    class CloseCommand : ICommand
+    {
+        private readonly DomainData domainData;
+        private readonly ConsoleView consoleView;
+
+        public CloseCommand(DomainData domainData, ConsoleView consoleView)
+        {
+            if (domainData == null) throw new ArgumentNullException("domainData");
+            if (consoleView == null) throw new ArgumentNullException("consoleView");
+
+            this.domainData = domainData;
+            this.consoleView = consoleView;
+        }
+
+        public void Execute(CommandInfo commandInfo)
+        {
+            if (commandInfo == null) throw new ArgumentNullException("commandInfo");
+
+            if (domainData.AddressBook != null)
+            {
+                domainData.CloseAddressBook();
+                consoleView.WriteInfo("Address book was closed.");
+            }
+            else
+            {
+                consoleView.WriteInfo("No address book is oppened.");
+            }
+        }
+    }
+}
