49db254a103a4688235080403bfede352db5c04b
lastunicorn <coldblackstar@yahoo.com>
2016-05-14 19:12:22 +0300

    added a gitignore file in the root

---
+++ sources/Lisimba.ZipXmlGate/ByteArrayConverter.cs
@@ -0,0 +1,51 @@
+// Lisimba
+// Copyright (C) 2007-2016 Dust in the Wind
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace DustInTheWind.Lisimba.ZipXmlGate
+{
+    internal static class ByteArrayConverter
+    {
+        private static readonly uint[] Lookup32 = CreateLookup32();
+
+        private static uint[] CreateLookup32()
+        {
+            uint[] result = new uint[256];
+
+            for (int i = 0; i < 256; i++)
+            {
+                string s = i.ToString("x2");
+                result[i] = ((uint)s[0]) + ((uint)s[1] << 16);
+            }
+
+            return result;
+        }
+
+        public static string ToHexString(this byte[] bytes)
+        {
+            uint[] lookup32 = Lookup32;
+            char[] result = new char[bytes.Length * 2];
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                var val = lookup32[bytes[i]];
+                result[2 * i] = (char)val;
+                result[2 * i + 1] = (char)(val >> 16);
+            }
+
+            return new string(result);
+        }
+    }
+}
